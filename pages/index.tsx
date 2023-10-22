b533361dd52f21f35c5f82e6e59887a880599031
Furret <138615245+accumulatown@users.noreply.github.com>
2023-10-22 11:32:31 -0600

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  return (
+    <>
+      <img
+        className="maple-icon"
+        src={"/icons/black_mage.png"}
+      />
+      <h1>Hello World!</h1>
+    </>
+  )
+}
