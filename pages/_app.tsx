ebdfa1eafe37a708199dfdef47cf8d7e32c01f95
Lire <138615245+eryuelle@users.noreply.github.com>
2023-11-05 01:17:21 -0600

    Add burger menu for navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import { useState } from 'react';
 
 import '../public/fonts.css';
 import './_app.css';
@@ -7,6 +8,20 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <div id="body">
+        <div className="burger-menu">
+          <input type="checkbox" />
+          <div className="burger-lines">
+            <span className="line line1"></span>
+            <span className="line line2"></span>
+            <span className="line line3"></span>
+          </div>  
+
+          <div className="menu">
+            <li><a className="menu-item" href="/">Home</a></li>
+            <li><a className="menu-item" href="/bosses">Bosses</a></li>
+          </div>
+        </div>
+        
         <div id={"container"}>
           <Component {...pageProps} />
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 export default function Index() {
   return (
     <>
+      <h1>Dailies</h1>
+      <div>Resets at 6pm MT</div>
       <img
         className="maple-icon"
         src={"/icons/black_mage.png"}
       />
-      <h1>Hello World!</h1>
     </>
   )
 }
