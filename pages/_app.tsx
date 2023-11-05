aa5223e051ac1c25fbd048f1c3f079c37728e55a
Lire <138615245+eryuelle@users.noreply.github.com>
2023-11-05 01:28:38 -0600

    Collapse menu with outside click

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
 import { AppProps } from 'next/app';
-import { useState } from 'react';
+import { useRef } from 'react';
 
 import '../public/fonts.css';
 import './_app.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
+  const pageClick = () => {
+    if (checkboxRef === null || checkboxRef.current === null) {
+      return;
+    }
+    checkboxRef.current.checked = false;
+  };
+
   return (
     <>
       <div id="body">
         <div className="burger-menu">
-          <input type="checkbox" />
+          <input type="checkbox" ref={checkboxRef} />
           <div className="burger-lines">
             <span className="line line1"></span>
             <span className="line line2"></span>
@@ -21,9 +30,11 @@ export default function App({ Component, pageProps }: AppProps) {
             <li><a className="menu-item" href="/bosses">Bosses</a></li>
           </div>
         </div>
-        
-        <div id={"container"}>
-          <Component {...pageProps} />
+
+        <div id="page" onClick={pageClick}>
+          <div id="container">
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
     </>
