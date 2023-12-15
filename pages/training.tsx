224e97fefab7301b960911a368b90551e907405d
Lire <138615245+eryuelle@users.noreply.github.com>
2023-12-15 16:06:11 -0700

    Training revamp, part 1

--- pages/training.tsx
+++ pages/training.tsx
@@ -1,38 +1,152 @@
 import TabRow from "../src/components/tab-row/tab-row";
 
+interface Map {
+  name: string;
+  range: string;
+  force?: string;
+}
+
+interface TrainingTableProps {
+  maps: Map[];
+}
+
+const maps = [
+  {
+    name: "Character Quest",
+    range: "1~30",
+  },
+  {
+    name: "Savage Terminal Maps",
+    range: "1~30",
+  },
+  {
+    name: "Theme Dungeons",
+    range: "30~60",
+  },
+  {
+    name: "Silent Swamp",
+    range: "66",
+  },
+  {
+    name: "Cave Cliff",
+    range: "68",
+  },
+  {
+    name: "Stairway to the Sky I",
+    range: "71",
+  },
+  {
+    name: "Ice Valley II",
+    range: "81",
+  },
+  {
+    name: "Tent of the Entertainers",
+    range: "86",
+  },
+  {
+    name: "Sahel 2",
+    range: "90",
+  },
+  {
+    name: "Lab - Area C-2: Authorized Personnel Only",
+    range: "95",
+  },
+  {
+    name: "Minar Forest: West Border",
+    range: "104",
+  },
+  {
+    name: "Toy Factory <Process 1> Zone 5",
+    range: "113",
+  },
+  {
+    name: " Fantasy Theme World: Extreme Station <3>",
+    range: "123",
+  },
+  {
+    name: "Korean Folk Town",
+    range: "130~140",
+  },
+  {
+    name: "Partem: Keeper's Path 4 (req. lv 135)",
+    range: "142",
+  },
+  {
+    name: "Kerning Tower: 2F Cafe <2, 3> (has SF)",
+    range: "149",
+  },
+  {
+    name: "Kerning Tower: 5F Cosmetic Shops <3> (has SF)",
+    range: "155",
+  },
+  {
+    name: "Showa Street 2",
+    range: "164",
+  },
+  {
+    name: "Boswell Field IV",
+    range: "167",
+  },
+  {
+    name: "Kritias: Forest of [Pain 2, Scorching 2, Sorrow 4]",
+    range: "170~186",
+  },
+  {
+    name: "Corridor 203 (has SF)",
+    range: "175",
+  },
+  {
+    name: "Dingy Brawl 'n' Bash Club 3 (req. level 175)",
+    range: "182",
+  },
+  {
+    name: "Twilight Perion: Deserted Southern Ridge (has SF)",
+    range: "190",
+  },
+  {
+    name: " Fox Tree Top Path (req. lv 180 + quests)",
+    range: "191",
+  },
+  {
+    name: "Fox Tree Lower Path (req. lv 180 + quests)",
+    range: "194",
+  },
+  {
+    name: "Twilight Perion: Forsaken Excavation Site 2",
+    range: "195",
+  },
+  {
+    name: "[Haven] Scrapyard Quests",
+    range: "199",
+  },
+];
+
+function TrainingTable({ maps }: TrainingTableProps) {
+  const iter = () => {
+    return maps.map((map) => {
+      return (
+        <tr>
+          <td className="padding-2 text-center">{map.range}</td>
+          <td className="padding-2">{map.name}</td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <table className="tmargin-8">
+      <thead className="bordered-bottom bold">
+        <td className="w-100 padding-2 text-center">Level</td>
+        <td className="padding-2">Location</td>
+      </thead>
+      { iter() }
+    </table>
+  );
+};
+
 export default function Index() {
   const tabs = {
-    "Maple World": (
-      <ul>
-        <li>[1 ~ 30] Character Quest</li>
-        <li>[1 ~ 30] Savage Terminal Maps</li>
-        <li>[30 ~ 60] Theme Dungeons</li>
-        <li>[66] Silent Swamp</li>
-        <li>[68] Cave Cliff</li>
-        <li>[71] Stairway to the Sky I</li>
-        <li>[81] Ice Valley II</li>
-        <li>[86] Tent of the Entertainers</li>
-        <li>[90] Sahel 2</li>
-        <li>[95] Lab - Area C-2: Authorized Personnel Only</li>
-        <li>[104] Minar Forest: West Border</li>
-        <li>[113] Toy Factory {"<Process 1>"} Zone 5</li>
-        <li>[123] Fantasy Theme World: Extreme Station {"<3>"}</li>
-        <li>[130 ~ 140] Korean Folk Town</li>
-        <li>[142] Partem: Keeper's Path 4 (req. lv 135)</li>
-        <li>[149] Kerning Tower: 2F Cafe {"<2, 3>"} (req. lv 145, has SF)</li>
-        <li>[155] Kerning Tower: 5F Cosmetic Shops {"<3>"} (req. lv 145)</li>
-        <li>[164] Showa Street 2</li>
-        <li>[167] Boswell Field IV</li>
-        <li>[170 ~ 186] Kritias: Forest of [Pain 2, Scorching 2, Sorrow 4]</li>
-        <li>[175] Corridor 203 (has SF)</li>
-        <li>[182] Dingy Brawl 'n' Bash Club 3 (req. level 175)</li>
-        <li>[190] Twilight Perion: Deserted Southern Ridge (has SF)</li>
-        <li>[191] Fox Tree Top Path (req. lv 180 + quests)</li>
-        <li>[194] Fox Tree Lower Path (req. lv 180 + quests)</li>
-        <li>[195] Twilight Perion: Forsaken Excavation Site 2</li>
-        <li>[199] Scrapyard Quests</li>
-      </ul>
-    ),
+    "Maple World": <TrainingTable maps={maps} />,
     "Arcane River": (
       <ul>
         <li>[201 (30*)] Weathered Land of Rage</li>
@@ -54,7 +168,7 @@ export default function Index() {
         <li>[254 (730*)] Void Current 3</li>
         <li>[263 (880*)] End of the World 1-4, 1-5, 1-7</li>
       </ul>
-    ), 
+    ),
   };
 
   return (
